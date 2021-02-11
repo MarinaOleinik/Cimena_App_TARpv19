4338bed922dc3ec73c95345a8862e707d4d79156
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2021-02-11 10:42:06 +0200

    Read ja kohad loeme andmebaasist

--- Cimena_App/Program.cs
+++ Cimena_App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -8,25 +9,13 @@ namespace Cimena_App
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-
+ 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Start_app());
-
-
-            //using (var ctx = new CimenaDBContext())
-            //{
-            //    var film = new Film() { Filmi_nimetus = "New film" };
-
-            //    ctx.Films.Add(film);
-            //    ctx.SaveChanges();
-            //}
 
 
         }
--- Cimena_App/Start_app.cs
+++ Cimena_App/Start_app.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,43 +13,56 @@ namespace Cimena_App
 {
     public partial class Start_app : Form
     {
+        SqlConnection con;
+        //SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\marina.oleinik\projects\Cimena_App\Cimena_App\CinemaDB.mdf;Integrated Security=True");
+        
+        SqlDataAdapter Saalide_adapter;
         public int i=0, j=0;
         ListBox saalide_list;
+        int[] read_list;
+        int[] kohad_list;
         public Start_app()
-        {
-            this.Text = "SuperKino";
+        {   this.Text = "SuperKino";
             this.BackgroundImage = Image.FromFile(@"..\..\tere.png");
             this.Size = new Size(360, 300);
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\AppData\CinemaDB.mdf;Integrated Security=True");
+            con.Open();
+            
+            Saalide_adapter = new SqlDataAdapter("SELECT * FROM Saalid_tabel", con);
+            DataTable salid_tabel = new DataTable();
+            Saalide_adapter.Fill(salid_tabel);
             saalide_list = new ListBox();
             saalide_list.Location = new Point(10, 10);
             saalide_list.Font= new Font(DefaultFont.FontFamily, 14);
             saalide_list.Size = new Size(150, 30);
-            saalide_list.Items.Add("Vali saal");
-            saalide_list.Items.Add("Väike");
-            saalide_list.Items.Add("Keskmine");
-            saalide_list.Items.Add("Suur");
+                       
+            foreach (DataRow row in salid_tabel.Rows)
+            {
+                saalide_list.Items.Add(row["Saalinimetus"]);
+            }
+
+            read_list = new int[salid_tabel.Rows.Count];
+            kohad_list = new int[salid_tabel.Rows.Count];
+            int a = 0;
+            foreach (DataRow row in salid_tabel.Rows)
+            {
+                read_list[a] = (int)row["Read"];
+                kohad_list[a] = (int)row["Kohad"];
+                a=a+1;
+            }
+            con.Close();
+            //saalide_list.Items.Add("Vali saal");
+            //saalide_list.Items.Add("Väike");
+            //saalide_list.Items.Add("Keskmine");
+            //saalide_list.Items.Add("Suur");
             this.Controls.Add(saalide_list);
             saalide_list.SelectedIndexChanged += Saalide_list_SelectedIndexChanged;
         }
 
         private void Saalide_list_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (saalide_list.SelectedIndex == 1)
-            {
-                i = 5; j = 5;
-            }
-            else if (saalide_list.SelectedIndex == 2)
-            {
-                i = 10; j = 10;
-            }
-            else if (saalide_list.SelectedIndex == 3)
-            {
-                i = 15; j = 15;
-            }
-            else
-            {
-                MessageBox.Show("Viga!", "Vaja saal valida");
-            }
+            i = read_list[saalide_list.SelectedIndex]; 
+            j = kohad_list[saalide_list.SelectedIndex];
             Saalid saalid = new Saalid(i, j);
             saalid.Show();
         }
